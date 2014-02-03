22a9ce62b35f206db60a4db14958670b8ef1225c
Sumeet Singh <ortuna@gmail.com>
2014-02-03 15:27:02 -0500

    Change up design

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -1,6 +1,2 @@
 $(document).ready(function() {
-  $("#information .box").each(function(index, element) {
-    $(element).addClass("animated flipInY");
-    $(element).removeClass("hidden");
-  });
 });
