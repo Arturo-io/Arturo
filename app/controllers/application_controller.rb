c4c7d8279c10845525963fad5eca3b955a91fa9b
Sumeet Singh <ortuna@gmail.com>
2014-02-05 22:20:02 -0500

    Updated gems

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   def current_user(sess = session)
     return nil unless sess[:user_id]
-    User.find(sess[:user_id])
+    @user ||= User.find(sess[:user_id])
   end
 end
