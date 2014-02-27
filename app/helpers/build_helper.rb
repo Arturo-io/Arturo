407ffbdf04792bbef790a03eaa58e707a5e4b2c3
Sumeet Singh <ortuna@gmail.com>
2014-02-26 21:09:37 -0500

    Cleaned up build partials

--- app/helpers/build_helper.rb
+++ app/helpers/build_helper.rb
@@ -1,6 +1,22 @@
 module BuildHelper
+  def link_to_repo(build)
+    link_to "#{build.repo.name} ##{build.id}", 
+      url_for(controller: 'build', action: 'show', 
+              id: build[:id], only_path: true)
+  end
+
+  def link_to_commit(build)
+    link_to limit_string(build.commit, 8), 
+            build.commit_url, 
+            target: '_blank'
+  end
+
+  def branch_name(branch)
+    limit_string(branch, 8)
+  end
+
   def message(message)
-    (message && message[0..49] || "")
+    limit_string(message, 49)
   end
 
   def status_has_spinner?(status)
@@ -13,5 +29,12 @@ module BuildHelper
   def build_status(status)
     output = status_has_spinner?(status) ? fa_icon("spinner spin") : ""
     output << " #{status}"
+  end
+
+  private
+  def limit_string(string, max)
+    return "" unless string
+    return string if string.length < max
+    string[0..max]  << "..."
   end
 end
