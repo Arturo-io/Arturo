8e5084140d1aafd4f61160b4f8efb79aead142a3
Sumeet Singh <ortuna@gmail.com>
2014-03-18 22:07:34 -0400

    Added some transitions to the homepage

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -1,2 +1,13 @@
 $(document).ready(function() {
+  $(".feature").hover(function(e){
+    $(this).children(".icon").transition({scale: 1.1}, 500, 'snap');
+  }, function(e){ 
+    $(this).children(".icon").transition({scale: 1.0}, 500, 'snap');
+  });
+
+  $(".pricing").hover(function(e){
+    $(this).transition({ scale: 1.1 }, 500, 'snap');
+  }, function(e){ 
+    $(this).transition({ scale: 1.0 }, 500, 'snap');
+  });
 });
