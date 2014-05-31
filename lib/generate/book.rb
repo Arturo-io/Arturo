b357424c4d3a2e21443640bed78226125a1ed166
Sumeet Singh <ortuna@gmail.com>
2014-05-30 23:23:41 -0400

    Only check private builds

--- lib/generate/book.rb
+++ lib/generate/book.rb
@@ -1,10 +1,13 @@
 class Generate::Book 
+  class PrivateRepoLimitReached < StandardError; end
+
   def initialize(build_id, formats)
     @build_id = build_id
     @formats  = formats
   end
 
   def execute
+    check_build_limit if private_repo?
     builder.new(@build_id, formats: @formats).execute
   end
 
@@ -21,15 +24,33 @@ class Generate::Book
   end
 
   def lookup_manifest
-    build  = Build.find(@build_id)
-    sha    = build[:commit]
-    client = client(build.user[:auth_token])
+    @build ||= ::Build.find(@build_id)
+    sha    = @build[:commit]
+    client = client(@build.user[:auth_token])
     Generate::Manifest
-      .new(build.repo[:full_name], sha, client)
+      .new(@build.repo[:full_name], sha, client)
       .has_manifest?
   end
 
   private
+  def private_repo?
+    @build ||= ::Build.find(@build_id)
+    @build.repo.private
+  end
+  
+  def check_build_limit
+    @build ||= ::Build.find(@build_id)
+    raise_error unless @build.user.within_repo_limit?
+  end
+
+  def raise_error
+    message = <<-eos
+You have reached the private repository limit on your account.
+Please unfollow some repositories or upgrade your account. 
+eos
+    raise PrivateRepoLimitReached, message
+  end
+
   def client(auth_token)
     Octokit::Client.new(access_token: auth_token)
   end
