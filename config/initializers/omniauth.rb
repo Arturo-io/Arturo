c6844ff2702400a13206c7be0831600dc13a6c66
Sumeet Singh <ortuna@gmail.com>
2014-05-30 21:20:10 -0400

    Better scope for OAuth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, 
            Rails.application.config.github_key,
            Rails.application.config.github_secret,
-           scope: 'user, repo'
+           scope: 'user:email, repo, read:repo_hook, write:repo_hook'
 end
